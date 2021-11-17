f6e108a2926c76fec40578906a9a9ee01f31c3e3
crangoren <crangoren@gmail.com>
2021-11-17 20:17:58 +0300

    Initial commit

---
+++ src/main/java/RKSuleimanov/springweb_hw/SpringWebHwApplication.java
@@ -0,0 +1,13 @@
+package RKSuleimanov.springweb_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RKSuleimanov/springweb_hw/SpringWebHwApplicationTests.java
@@ -0,0 +1,13 @@
+package RKSuleimanov.springweb_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
